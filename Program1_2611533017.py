45cbcb8e9e7fe7b69b023b45e329ba7db581c188
FadelMafahl <felz.faa78@gmail.com>
2026-09-05 16:34:25 +0700

    Program 1

---
+++ Program1_2611533017.py
@@ -0,0 +1,12 @@
+# program pertama
+"""
+Komentar
+dua baris atau lebih
+pertemuan 1 praktikum alpro
+"""
+print("Hello world")
+print()
+print("Departemen Informatika")
+print("Universitas Andalas")
+print("/\\//\\\\///\\\\\\")
+print("program ini" + "dibuat oleh" + "FadelMN_2611533017.")
