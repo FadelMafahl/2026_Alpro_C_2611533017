84f3e7cd980769a562537454b1d0c58c38652762
FadelMafahl <felz.faa78@gmail.com>
2026-09-10 11:17:11 +0700

    Praktikum pekan 2

---
+++ Fadel String_2611533017.py
@@ -0,0 +1,12 @@
+nama= "Fadel_3017"
+jenis_kelamin= "Laki-laki"
+alamat = """"
+    Kampus Unand 
+    kec.nanggalo
+    kota padang
+"""
+agama= "islam"
+print("Nama saya adalah", nama)
+print("Jenis kelamin saya adalah", jenis_kelamin)
+print("Alamat saya adalah", alamat)
+print("Agama saya adalah", agama)
