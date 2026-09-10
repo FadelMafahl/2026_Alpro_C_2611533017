b1575428dadab46505dc31754ead5c3c42de2dec
FadelMafahl <felz.faa78@gmail.com>
2026-09-10 10:59:26 +0700

    Praktikum pekan 2

---
+++ Fadel Numerik_2611533017.py
@@ -0,0 +1,11 @@
+#create a variable with numeric type value.
+a_3017=100
+print("The type of a variable having value",a_3017,"is",type(a_3017))
+
+#create a variable with float value.
+b_3017=10.2345
+print("The type of variable having value",b_3017,"is",type(b_3017))
+
+#create a variable with complex value.
+c_3017=100+3j
+print("The type of variable having value",c_3017,"is",type(c_3017))
